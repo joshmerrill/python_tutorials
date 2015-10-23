cffbdc2176e369b8a4de109f254ec77ea34e305e
joshmerrill <joshmerrill@gmail.com>
2015-10-23 16:43:13 -0400

    Added new examples

---
+++ ex8.py
@@ -0,0 +1,13 @@
+# define variables
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't last sing.",
+	"So I said goodnight."
+	)
+	
---
+++ ex9.py
@@ -0,0 +1,15 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tues Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"	# \n adds a new line before the following text.
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# the triple quotes allow you to add multiple lines to be printed in the format you code it.
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like. 
+Weven 4 lines if we want, or 5, or 6.
+"""
