c8eb5ee29e88fd26d849dc70e514aaea7a3c0473
Josh Merrill <joshmerrill@gmail.com>
2015-10-24 19:43:08 -0400

    Added more exercises

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import test
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "The first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
